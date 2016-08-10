3e555ea602758f2c7e1ef9dfbbeb6681a871bb46
Alex <alexmoreira91@gmail.com>
2016-08-10 11:39:25 -0300

    Reparado erro no foreach e renomeado arquivo para request.php

--- aula02/request.php
+++ aula02/request.php
@@ -19,7 +19,7 @@ class Request
 	public function __set($atrib, $value)
 	{
 	
-		$this->$atrib->$value;
+		$this->$atrib = $value;
 	}
 
 	public function __get($atrib)
@@ -30,12 +30,12 @@ class Request
 	public function toString()
         {
                 $request = $this->protocol.'://'.$this->ip.'/'.$this->resource.'?';
-                foreach($this->parameters as $key=>$value)
+                foreach($this->parameter as $key=>$value)
                         {
                                 $request = $request.$key.'='.$value.'&amp';
                         }
                 return substr($request,0,-4);	
 	}
 }
-$request = new Request('POST','http','172.22.51.17','resource',array('p1'=>'primeiro','p2'=>'segundo'));
+$request = new Request('POST','http','172.22.51.17','resource',array('p1'=>'para','p2'=>'me', 'p3'=>'tro'));
 echo $request->toString();
