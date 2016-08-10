0c3c21a0f0eaed7af72de44d4eceeaf81ba8270d
Alex <alexmoreira91@gmail.com>
2016-08-09 23:01:22 -0300

    Criado get set

---
+++ aula02/index.php
@@ -0,0 +1,41 @@
+<?php
+
+class Request
+{
+	private $protocol;
+	private $ip;
+	private $resource;
+	private $parameter;
+	private $method;
+
+	public function __construct($method, $protocol, $ip, $resource, $parameter) {
+		$this->protocol = $protocol;
+		$this->ip = $ip;
+		$this->resource = $resource;
+		$this->parameter = $parameter;
+		$this->method = $method;
+	}
+
+	public function __set($atrib, $value)
+	{
+	
+		$this->$atrib->$value;
+	}
+
+	public function __get($atrib)
+	{
+		return $this->$atrib;
+	}
+
+	public function toString()
+        {
+                $request = $this->protocol.'://'.$this->ip.'/'.$this->resource.'?';
+                foreach($this->parameters as $key=>$value)
+                        {
+                                $request = $request.$key.'='.$value.'&amp';
+                        }
+                return substr($request,0,-4);	
+	}
+}
+$request = new Request('POST','http','172.22.51.17','resource',array('p1'=>'primeiro','p2'=>'segundo'));
+echo $request->toString();
